0503c6d504464cda698d42a7c9bbabbb53fa4dde
Pontus Backman <pontus98@hotmail.com>
2015-03-11 00:39:07 +0100

    gallery now working

--- js/gallery.js
+++ js/gallery.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 	opacity_changer();
+	popup_image();
 });
 
 function opacity_changer() {
@@ -7,5 +8,30 @@ function opacity_changer() {
 		$(this).fadeTo("400", 1);
 	}, function() {
 		$(this).fadeTo("400", 0.5);
+	});
+
+	$("#xclose").hover(function() {
+		$("#xclose").fadeTo("400", 1);
+	}, function() {
+		$("#xclose").fadeTo("400", 0.7);
+	});
+}
+
+function popup_image() {
+	var currentImg;
+
+	$(".thumbnail").click(function() {
+		currentImg = $(this).attr('id');
+		console.log(currentImg);
+
+		$(".popup").append('<img class="image" src="pics/' + currentImg + '.jpg">')
+		$(".gallery").fadeOut(500);
+		$(".popup").fadeIn(1000);
+	});
+
+	$("#xclose").click(function(event) {
+		$(".popup").fadeOut(1000);
+		$(".gallery").fadeIn(500);
+		$(".image").remove();
 	});
 }
