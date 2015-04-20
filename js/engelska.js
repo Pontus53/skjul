ed8024d877852d9f730fa395318307d5f1c10c33
Pontus53 <pontus98@hotmail.com>
2015-04-20 15:58:55 +0200

    mer översättning

--- js/engelska.js
+++ js/engelska.js
@@ -17,5 +17,7 @@ function engelska() {
 		$("#F1D").html("Our shed has enough space for just your lawn mower. Let our shed take care of the charging and lawn mowing while you relax and enjoy the wonderful climate.");
 		$("#F2").html('Cylindrical storage. <span class="muted">A lot of space for your tools.</span>');
 		$("#F2D").html("With our smart storage solutions is space no longer a problem. All your tools and equipment will find its place in our shed.");
+		$("#F3").html('Solar panels. <span class="muted">For a sustainable future.</span>');
+		$("#F3D").html("We think about the future and coming generations ability to sustain their needs. With our smart solar panels no other energy source is needed. Your robotic lawn mower will charge itself with green energy.");
 	}
 }
