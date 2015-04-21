fdbcc59ba7c80b4efb3e342c8a07eaec01eefd53
Pontus53 <pontus98@hotmail.com>
2015-04-21 14:08:18 +0200

    lagt till svenska switch

--- js/engelska.js
+++ js/engelska.js
@@ -19,5 +19,31 @@ function engelska() {
 		$("#F2D").html("With our smart storage solutions is space no longer a problem. All your tools and equipment will find its place in our shed.");
 		$("#F3").html('Solar panels. <span class="muted">For a sustainable future.</span>');
 		$("#F3D").html("We think about the future and coming generations ability to sustain their needs. With our smart solar panels no other energy source is needed. Your robotic lawn mower will charge itself with green energy.");
+		$("#BACK").html("Return to the top");
+	}
+}
+
+function svenska() {
+	if (document.title = "Home &middot TomorrowShed") {
+		$("title").html("Hem &middot TomorrowShed");
+		$("#H1").html("Hem");
+		$("#H2").html("Om oss");
+		$("#H3").html("Kontakta oss");
+		$("#VIDARE1").html("Vidare &raquo");
+		$("#VIDARE2").html("Vidare &raquo");
+		$("#VIDARE3").html("Vidare &raquo");
+		$("#L1").html("Bilder");
+		$("#L1D").html("Fler bilder av skjulet, insida och utsida.");
+		$("#L2").html("Specifikationer");
+		$("#L2D").html("Lär dig allt du behöver veta om vårat trädgårdsskjul.");
+		$("#L3").html("Pris");
+		$("#L3D").html("Vad kostar detta skjul?");
+		$("#F1").html('Robotgräsklippare. <span class="muted">Din gräsmatta har aldrig klippt sig själv så lätt.</span>');
+		$("#F1D").html("Vårt skjul har plats för just din robotgräsklippare. Låt skjulet sköta laddning och gräsklippande medan du tar det lugnt och njuter av det underbara klimatet.");
+		$("#F2").html('Cylindrisk förvaring. <span class="muted">Massor med plats för dina verktyg.</span>');
+		$("#F2D").html("Med våra smarta förvaringslösningar är utrymme inte längre något problem. Alla dina verktyg och tillbehör får lätt plats någonstans bland alla förvaringsplatser.");
+		$("#F3").html('Solpaneler. <span class="muted">För en hållbar utveckling.</span>');
+		$("#F3D").html("Vi tänker på miljön och kommande generationers möjligheter att tillfredställa sina behov. Med våra smarta solpaneler behövs ingen annan strömförsörjning. Din robotgräsklippare kommer laddas med grön energi.");
+		$("#BACK").html("Tillbaka upp");
 	}
 }
