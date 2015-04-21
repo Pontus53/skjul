9d0f4e740dc8b5687b16f1779b1854b8a4dc98b4
Pontus53 <pontus98@hotmail.com>
2015-04-21 13:41:14 +0200

    new file

---
+++ js/engelska.js
@@ -0,0 +1,22 @@
+function engelska() {
+	if (document.title = "Hem &middot TomorrowShed") {
+		$("title").html("Home &middot TomorrowShed");
+		$("#H1").html("Home");
+		$("#H2").html("About us");
+		$("#H3").html("Contact");
+		$("#VIDARE1").html("Continue &raquo");
+		$("#VIDARE2").html("Continue &raquo");
+		$("#VIDARE3").html("Continue &raquo");
+		$("#L1").html("Pictures");
+		$("#L1D").html("More pictures of the shed, inside and outside.");
+		$("#L2").html("Specifications");
+		$("#L2D").html("Everything you need to know about our shed.");
+		$("#L3").html("Price");
+		$("#L3D").html("What's the price of the shed?");
+		$("#F1").html('Robotic lawn mower. <span class="muted">Your lawn will take care of itself.</span>');
+		$("#F1D").html("Our shed has enough space for just your lawn mower. Let our shed take care of the charging and lawn mowing while you relax and enjoy the wonderful climate.");
+		$("#F2").html('Cylindrical storage. <span class="muted">A lot of space for your tools.</span>');
+		$("#F2D").html("With our smart storage solutions is space no longer a problem. All your tools and equipment will find its place in our shed.");
+		$("#F3").html('Solar panels. <span class="muted">For a sustainable future.</span>');
+		$("#F3D").html("We think about the future and coming generations ability to sustain their needs. With our smart solar panels no other energy source is needed. Your robotic lawn mower will charge itself with green energy.");
+	}
